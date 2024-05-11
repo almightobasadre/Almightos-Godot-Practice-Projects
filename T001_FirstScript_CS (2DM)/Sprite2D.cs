c4dbcf89e45c54dbb3f39c3ae06485c75b10c5e4
almightobasadre <alau.basadre.up@phinmaed.com>
2024-05-11 18:05:34 +0800

    Created my first CSharp script

---
+++ T001_FirstScript_CS (2DM)/Sprite2D.cs
@@ -0,0 +1,10 @@
+using Godot;
+using System;
+
+public partial class Sprite2D : Godot.Sprite2D
+{
+    public Sprite2D()
+    {
+        GD.Print("Hello, World!");
+    }
+}
--- T001_FirstScript_GD (2DM)/Sprite2D.gd
+++ T001_FirstScript_GD (2DM)/Sprite2D.gd
@@ -1,4 +1,14 @@
 extends Sprite2D
 
-func _init():
-	print("Hello, World!")
+var speed = 400
+var angular_speed = PI
+
+func _process(delta):
+	rotation += angular_speed * delta
+	
+	var velocity = Vector2.UP.rotated(rotation) * speed
+	
+	position += velocity * delta
+
+#func _init():
+	#print("Hello, World!")
