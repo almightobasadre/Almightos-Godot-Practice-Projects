26edf9328d7c314daa2eda519b8a7007d7201c28
almightobasadre <alau.basadre.up@phinmaed.com>
2024-05-18 18:06:33 +0800

    Added direction to sprite rotation

--- T004_CustomSignals_GD (2DM)/scripts/enemy_gd.gd
+++ T004_CustomSignals_GD (2DM)/scripts/enemy_gd.gd
@@ -6,10 +6,13 @@ var radius = Vector2(500.0, 200.0)
 var angle = 0.0
 
 @onready var start_position = position
+@onready var previous_position = start_position
 
 func _process(delta):
 	angle = wrapf(angle + angular_speed * delta, 0.0, 2 * angular_speed)
 	
 	var direction = Vector2(cos(angle), sin(2*angle))
 	
+	previous_position = position
 	position = start_position + direction * radius
+	rotation = (position - previous_position).angle()
